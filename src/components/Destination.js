7d0c6910a35377335cfc0e2d104914520f987c18
chaitanya091 <chaitanyapatilcr7@gmail.com>
2023-03-18 16:56:06 +0530

    Dynamic destination Component

--- src/components/Destination.js
+++ src/components/Destination.js
@@ -4,7 +4,7 @@ import Mounting2 from "../assets/10.jpg";
 import Mounting3 from "../assets/11.jpg";
 import Mounting4 from "../assets/12.jpg";
 import "./DestinationStyles.css";
-
+import DestinationData from "./DestinationData";
 
 // Arrow Function
 const Destination = () =>{
@@ -12,21 +12,13 @@ const Destination = () =>{
         <div className="destination">
             <h1>Popular Destination</h1>
             <p>Tours give you the opportunity to see a lot, within a time frame</p>
-        
-        <div className="first-des">
-            <div className="des-text">
-            <h2>Taal Volcano, Batangas</h2>
-            <p>One of the most iconic views Luzon,Mt. Taal boasts a volcano inside a lake inside an island island. if you fancy closer look, the hike up to the crater is a mere 45 minutes, and is easy enough for begineers. Guides will assist you most of the way,and you'll see the peculiar environment found on an active volcano, including volcanic rocks and stream vents.The hike can be dusty and hot, so plan for an early morning trip, and then unwind with some bulalo before heading back home!</p>
-            </div>
-        
-        <div className="image">
-            <img alt="img" src={Mounting1}/>
-
-             
-            <img alt="img" src={Mounting2}/>    
-
-        </div>
-        </div>
+            <DestinationData
+            heading="Taal Volcano, Batangas"
+            text="One of the most iconic views Luzon,Mt. Taal boasts a volcano inside a lake inside an island island. if you fancy closer look, the hike up to the crater is a mere 45 minutes, and is easy enough for begineers. Guides will assist you most of the way,and you'll see the peculiar environment found on an active volcano, including volcanic rocks and stream vents.The hike can be dusty and hot, so plan for an early morning trip, and then unwind with some bulalo before heading back home!"
+            img1={Mounting1}
+            img2={Mounting2}
+           />
+         
         </div>
          
         
---
+++ src/components/DestinationData.js
@@ -0,0 +1,28 @@
+import { Component } from "react";
+import "./DestinationStyles.css";
+import Mounting1 from "../assets/9.jpg";
+import Mounting2 from "../assets/10.jpg";
+import Mounting3 from "../assets/11.jpg";
+import Mounting4 from "../assets/12.jpg";
+import Destination from "./Destination";
+class DestinationData extends Component{
+    render(){
+        return(
+            <div className="first-des">
+            <div className="des-text">
+            <h2>{this.props.heading}</h2>
+            <p>{this.props.text}</p>
+            </div> 
+        
+        <div className="image">
+            <img alt="img" src={this.props.img1}/>
+
+             
+            <img alt="img" src={this.props.img2}/>    
+
+        </div>
+        </div>
+        )
+    }
+}
+export default DestinationData
